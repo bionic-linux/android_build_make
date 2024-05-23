73326f27e55bd1a45c3a2d28de209b0875f9c0e9
Luca Farsi <lucafarsi@google.com>
2024-05-22 17:21:47 -0700

    Add optimized build features in build_test_suites
    Add the optimized build feature in build_test_suites. WIP
    
    Test: WIP
    Bug: 342264003
    Change-Id: I05a4ac4026c345f7ced771aa6deb1b6d1c38705c

---
+++ ci/optimized_targets.py
@@ -0,0 +1,63 @@
+#
+# Copyright 2024, The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from abc import ABC
+
+
+class OptimizedBuildTarget(ABC):
+
+  # def __init__(self, build_context, args, get_build_targets, package_outputs):
+  #  self.get_build_targets = get_build_targets
+  #  self.package_outputs = package_outputs
+
+  def __init__(self, build_context, args):
+    self.build_context = build_context
+    self.args = args
+
+  def get_build_targets(self):
+    pass
+
+  def package_outputs(self):
+    pass
+
+
+class NullOptimizer(OptimizedBuildTarget):
+
+  def __init__(self, target):
+    self.target = target
+
+  def get_build_targets(self):
+    return [self.target]
+
+  def package_outputs():
+    pass
+
+
+class OptimizedMts(OptimizedBuildTarget):
+
+  def get_build_targets(self):
+    return []
+
+  def package_outputs(self):
+    pass
+
+
+OPTIMIZED_BUILD_TARGETS = {
+    'mts': (
+        lambda build_context, args: NullOptimizer('mts')
+        if not 'optimize-mts' in build_context['enabled_build_features']
+        else OptimizedMts(build_context, args)
+    )
+}
