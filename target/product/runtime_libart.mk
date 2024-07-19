23ac879cc42d8e6aae10df2eacd042ce42b2f0a1
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-07-19 15:40:59 +0000

    Revert "Copy odex files from system_other to data/dalvik-cache o..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/013859cb-344a-4718-a9e4-165b0324ebb9). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ia7cbd2105bf8621c8479e1331d679f8624a2687d
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- target/product/runtime_libart.mk
+++ target/product/runtime_libart.mk
@@ -178,6 +178,3 @@ PRODUCT_SYSTEM_PROPERTIES += \
 PRODUCT_SYSTEM_PROPERTIES += \
     dalvik.vm.useartservice=true \
     dalvik.vm.enable_pr_dexopt=true
-
-# Copy preopted files from system_b on first boot.
-PRODUCT_SYSTEM_PROPERTIES += ro.cp_system_other_odex=1
