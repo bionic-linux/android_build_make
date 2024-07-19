8047550e6fa8f261b31349debe51cfbca301d708
Sid Nayyar <sidnayyar@google.com>
2024-07-19 14:26:16 +0000

    Add kernel lifetimes and approved OGKI builds configs to GSI
    These kernel configs are required for end-of-life enforcement and OGKI
    compliance verification VTS tests.
    
    Bug: 338636324
    Bug: 342094847
    Test: manually built and flashed on device, device had config files
    Change-Id: Ia1f94ba19bab1f2e3b129827ad5634023290dbc5

--- target/product/generic_system.mk
+++ target/product/generic_system.mk
@@ -153,3 +153,8 @@ $(call require-artifacts-in-path, $(_my_paths), $(_my_allowed_list))
 
 # Product config map to toggle between sources and prebuilts of required mainline modules
 PRODUCT_RELEASE_CONFIG_MAPS += $(wildcard vendor/google_shared/build/release/gms_mainline/required/release_config_map.textproto)
+
+# Include kernel configs.
+PRODUCT_PACKAGES += \
+    approved-ogki-builds.xml \
+    kernel-lifetimes.xml
