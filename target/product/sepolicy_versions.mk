a649573cb3c472a4b7bf13e437639026f2709a8e
kellyhung <kellyhung@google.com>
2024-07-23 04:16:32 +0000

    Replace product package selinux_policy_system with include selinux_policy_system.mk
    Bug: 350881031
    Test: Presubmit
    Change-Id: Id749e1e78c3f034feff0d14b9d8e9e3b5671abaf

---
+++ target/product/sepolicy_versions.mk
@@ -0,0 +1,24 @@
+# SEPolicy versions
+
+# PLATFORM_SEPOLICY_VERSION is a number of the form "YYYYMM" with "YYYYMM"
+# mapping to vFRC version.
+PLATFORM_SEPOLICY_VERSION ?= $(RELEASE_BOARD_API_LEVEL)
+BOARD_SEPOLICY_VERS ?= $(PLATFORM_SEPOLICY_VERSION)
+.KATI_READONLY := PLATFORM_SEPOLICY_VERSION BOARD_SEPOLICY_VERS
+
+$(warning selinux_policy_system.mk)
+$(warning PLATFORM_SEPOLICY_VERSION:$(PLATFORM_SEPOLICY_VERSION))
+$(warning BOARD_SEPOLICY_VERS:$(BOARD_SEPOLICY_VERS))
+
+# A list of SEPolicy versions, besides PLATFORM_SEPOLICY_VERSION, that the framework supports.
+PLATFORM_SEPOLICY_COMPAT_VERSIONS ?= $(filter-out $(PLATFORM_SEPOLICY_VERSION), \
+    29.0 \
+    30.0 \
+    31.0 \
+    32.0 \
+    33.0 \
+    34.0 \
+    202404 \
+    )
+.KATI_READONLY := \
+    PLATFORM_SEPOLICY_COMPAT_VERSIONS \
