e6ef29108f345b73fece2af4fdc3ed6d5436de6d
Herbert Xue <herbertxue@google.com>
2024-08-29 11:12:10 +0800

    Move COMPATIBILITY.tradefed_tests_dir to proper place
    Bug: 341873065
    Test: presubmit
    Change-Id: Ibd99b07b8548eafb72254fda2a248b9ec32ee8ef

--- core/tasks/tradefed-tests-list.mk
+++ core/tasks/tradefed-tests-list.mk
@@ -15,6 +15,12 @@
 # List all TradeFed tests from COMPATIBILITY.tradefed_tests_dir
 .PHONY: tradefed-tests-list
 
+COMPATIBILITY.tradefed_tests_dir := \
+  $(COMPATIBILITY.tradefed_tests_dir) \
+  tools/tradefederation/core/res/config \
+  tools/tradefederation/core/javatests/res/config
+
+$(warning "COMPATIBILITY", $(COMPATIBILITY.tradefed_tests_dir))
 tradefed_tests :=
 $(foreach dir, $(COMPATIBILITY.tradefed_tests_dir), \
   $(eval tradefed_tests += $(shell find $(dir) -type f -name "*.xml")))
