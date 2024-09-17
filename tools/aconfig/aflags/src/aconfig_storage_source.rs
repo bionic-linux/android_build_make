1fa7edc41479a53e0808307f5b4f5733e15ce214
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-09-17 10:28:05 +0000

    Revert "Show containers in list new storage"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/3e643ebc-27a1-4099-b95a-163b26241ac4). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ide9a452f862541dc9524d76c283ca26ea1ddbb0b
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- tools/aconfig/aflags/src/aconfig_storage_source.rs
+++ tools/aconfig/aflags/src/aconfig_storage_source.rs
@@ -1,4 +1,3 @@
-use crate::load_protos;
 use crate::{Flag, FlagSource};
 use crate::{FlagPermission, FlagValue, ValuePickedFrom};
 use aconfigd_protos::{
@@ -10,18 +9,13 @@ use anyhow::anyhow;
 use anyhow::Result;
 use protobuf::Message;
 use protobuf::SpecialFields;
-use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::Shutdown;
 use std::os::unix::net::UnixStream;
 
 pub struct AconfigStorageSource {}
 
-fn load_flag_to_container() -> Result<HashMap<String, String>> {
-    Ok(load_protos::load()?.into_iter().map(|p| (p.qualified_name(), p.container)).collect())
-}
-
-fn convert(msg: ProtoFlagQueryReturnMessage, containers: &HashMap<String, String>) -> Result<Flag> {
+fn convert(msg: ProtoFlagQueryReturnMessage) -> Result<Flag> {
     let (value, value_picked_from) = match (
         &msg.boot_flag_value,
         msg.default_flag_value,
@@ -61,21 +55,15 @@ fn convert(msg: ProtoFlagQueryReturnMessage, containers: &HashMap<String, String
         None => return Err(anyhow!("missing permission")),
     };
 
-    let name = msg.flag_name.ok_or(anyhow!("missing flag name"))?;
-    let package = msg.package_name.ok_or(anyhow!("missing package name"))?;
-    let qualified_name = format!("{package}.{name}");
     Ok(Flag {
-        name,
-        package,
+        name: msg.flag_name.ok_or(anyhow!("missing flag name"))?,
+        package: msg.package_name.ok_or(anyhow!("missing package name"))?,
         value,
         permission,
         value_picked_from,
         staged_value,
-        container: containers
-            .get(&qualified_name)
-            .cloned()
-            .unwrap_or_else(|| "<no container>".to_string())
-            .to_string(),
+        container: "-".to_string(),
+
         // TODO: remove once DeviceConfig is not in the CLI.
         namespace: "-".to_string(),
     })
@@ -126,13 +114,9 @@ fn read_from_socket() -> Result<Vec<ProtoFlagQueryReturnMessage>> {
 
 impl FlagSource for AconfigStorageSource {
     fn list_flags() -> Result<Vec<Flag>> {
-        let containers = load_flag_to_container()?;
         read_from_socket()
             .map(|query_messages| {
-                query_messages
-                    .iter()
-                    .map(|message| convert(message.clone(), &containers))
-                    .collect::<Vec<_>>()
+                query_messages.iter().map(|message| convert(message.clone())).collect::<Vec<_>>()
             })?
             .into_iter()
             .collect()
