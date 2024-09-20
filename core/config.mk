b0929f221631737836778c1af5b9ea47368ddd4e
Edward Liaw <edliaw@google.com>
2024-09-20 22:59:46 +0000

    Remove special case for ltp build rules
    LTP build rules are updated to use soong style dependencies, so these
    custom build logic can be removed.
    
    Bug: 277261121
    Test: mma
    Change-Id: I9c1207a8d2a0444ec935501087b9d6014f1839c4
    Signed-off-by: Edward Liaw <edliaw@google.com>

--- core/config.mk
+++ core/config.mk
@@ -1308,10 +1308,6 @@ endif
 SOONG_VARIABLES :=
 SOONG_EXTRA_VARIABLES :=
 
--include external/ltp/android/ltp_package_list.mk
-DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages)
-.KATI_READONLY := DEFAULT_DATA_OUT_MODULES
-
 include $(BUILD_SYSTEM)/dumpvar.mk
 
 ifdef BOARD_VNDK_VERSION
