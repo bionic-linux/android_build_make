3019a1cff24ae1b166b804f2bce5a0f215f6d269
Islam Elbanna <islamelbanna@google.com>
2024-09-26 14:15:23 +0000

    Use preloaded-classes at framework/base/boot/ instead of the combined one at framework/base/config
    This is to decouple the framework and art preloaded-classes everywhere.
    
    Bug: 260067246
    Change-Id: Iba0fb56496a93856b61f91085f6a0fda7216c57b
    Test: running `m`

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -517,7 +517,9 @@ PRODUCT_PACKAGES_DEBUG_JAVA_COVERAGE := \
     libdumpcoverage
 
 PRODUCT_COPY_FILES += $(call add-to-product-copy-files-if-exists,\
-    frameworks/base/config/preloaded-classes:system/etc/preloaded-classes)
+    frameworks/base/boot/preloaded-classes:system/etc/preloaded-classes)
+PRODUCT_COPY_FILES += $(call add-to-product-copy-files-if-exists,\
+    art/build/boot/preloaded-classes:system/etc/preloaded-classes-art)
 
 # Enable dirty image object binning to reduce dirty pages in the image.
 PRODUCT_PACKAGES += dirty-image-objects
