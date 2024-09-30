d52586a31685ba3a255937eeae164fd237d1402d
Bill Yang <yangbill@google.com>
2024-09-30 07:13:48 +0000

    Export libexynosscaler related variable to soong
    Export BOARD_USES_VENDORIMAGE, BOARD_USES_SCALER_M2M1SHOT, and
    BOARD_USES_ALIGN_RESTRICTION to soong.
    
    Bug: 370376532
    Test: m libexynosscaler libexynosgscaler
    Change-Id: I30da46a7b03338bc725c7529b23c7017812a6a15

--- core/android_soong_config_vars.mk
+++ core/android_soong_config_vars.mk
@@ -190,3 +190,8 @@ $(call soong_config_set_bool,camera,use_camera_v4l2_hal,$(if $(filter true,$(USE
 
 # Export target_board_platform to soong for hardware/google/graphics/common/libmemtrack:memtrack.$(TARGET_BOARD_PLATFORM)
 $(call soong_config_set,ANDROID,target_board_platform,$(TARGET_BOARD_PLATFORM))
+
+# Export board_uses_vendorimage to soong for hardware/google/graphics/common/libscaler:libexynosscaler
+$(call soong_config_set_bool,ANDROID,board_uses_vendorimage,$(if $(filter true,$(BOARD_USES_VENDORIMAGE)),true,false))
+$(call soong_config_set_bool,google_graphocs,board_uses_scaler_m2m1shot,$(if $(filter true,$(BOARD_USES_SCALER_M2M1SHOT)),true,false))
+$(call soong_config_set_bool,google_graphocs,board_uses_align_restriction,$(if $(filter true,$(BOARD_USES_ALIGN_RESTRICTION)),true,false))
