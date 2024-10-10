45dc951aa654722cbe076bea6d26a84c8b8a78b6
Marybeth Fair <marybethfair@google.com>
2024-10-10 09:39:16 -0400

    Return default instead of crash if package not found.
    Happening in tests right now.
    
    Test: atest
    Change-Id: If54b3176d1a871e3c4da3904c96cc9e995a4eeec

--- tools/aconfig/aconfig/src/codegen/java.rs
+++ tools/aconfig/aconfig/src/codegen/java.rs
@@ -515,6 +515,7 @@ mod tests {
         import android.compat.annotation.UnsupportedAppUsage;
         import android.provider.DeviceConfig;
         import android.provider.DeviceConfig.Properties;
+        import android.aconfig.storage.AconfigStorageException;
         import android.aconfig.storage.StorageInternalReader;
         import java.nio.file.Files;
         import java.nio.file.Paths;
@@ -531,15 +532,16 @@ mod tests {
             private static boolean enabledRw = true;
             private void init() {
                 StorageInternalReader reader = null;
+                boolean foundPackage = true;
                 try {
                     reader = new StorageInternalReader("system", "com.android.aconfig.test");
-                    disabledRw = reader.getBooleanFlagValue(1);
-                    disabledRwExported = reader.getBooleanFlagValue(2);
-                    enabledRw = reader.getBooleanFlagValue(8);
-                    disabledRwInOtherNamespace = reader.getBooleanFlagValue(3);
-                } catch (Exception e) {
-                    throw new RuntimeException("Cannot read flag in codegen", e);
+                } catch (AconfigStorageException e) {
+                    foundPackage = false;
                 }
+                disabledRw = foundPackage ? reader.getBooleanFlagValue(1) : false;
+                disabledRwExported = foundPackage ? reader.getBooleanFlagValue(2) : false;
+                enabledRw = foundPackage ? reader.getBooleanFlagValue(8) : true;
+                disabledRwInOtherNamespace = foundPackage ? reader.getBooleanFlagValue(3) : false;
                 isCached = true;
             }
             private void load_overrides_aconfig_test() {
