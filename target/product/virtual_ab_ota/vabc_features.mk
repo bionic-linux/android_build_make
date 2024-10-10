4b1ed855d5f400247407653606e8065c9a4646c0
Arthur Sfez <asfez@google.com>
2024-10-10 14:02:57 +0000

    Use optional assignments for some virtual_ab properties
    When using the normal assignment, it is impossible to override
    PRODUCT_VENDOR_PROPERTIES in a device config because the property
    will have two different values and the build system won't know
    which one to choose.
    
    This changes the io_uring and compression.xor assignments to
    optional because low memory devices may want to override these to
    relieve memory pressure when applying an OTA.
    
    Test: Manual, props remain true by default but can now be changed
    Bug: 341085461
    Change-Id: I3a49fb07b7a621c68a34aceeba1461970d2fba6a

--- target/product/virtual_ab_ota/vabc_features.mk
+++ target/product/virtual_ab_ota/vabc_features.mk
@@ -31,14 +31,15 @@ PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.enabled=true
 
 PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.enabled=true
 PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.userspace.snapshots.enabled=true
-PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.io_uring.enabled=true
-PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.xor.enabled=true
 PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.batch_writes=true
+
+# Optional assignments, low memory devices may benefit from overridding these.
+PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.io_uring.enabled?=true
+PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.xor.enabled?=true
+
 # Low memory device configurations. If memory usage and cpu utilization is
 # a bottleneck during OTA, the below configurations can be added to a
-# device's .mk file improve performance for low mem devices. Disabling
-# ro.virtual_ab.compression.xor.enabled and ro.virtual_ab.io_uring.enabled
-# is also recommended
+# device's .mk file improve performance for low mem devices.
 #
 # PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.read_ahead_size=16
 # PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.o_direct.enabled=true
@@ -50,6 +51,12 @@ PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.batch_writes=true
 # PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.cow_op_merge_size=16
 # PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.verify_threshold_size=1073741824
 # PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.verify_block_size=1048576
+#
+# Disabling ro.virtual_ab.compression.xor.enabled and ro.virtual_ab.io_uring.enabled
+# is also recommended.
+#
+# PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.io_uring.enabled=false
+# PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.xor.enabled=false
 
 # Enabling this property, will improve OTA install time
 # but will use an additional CPU core
